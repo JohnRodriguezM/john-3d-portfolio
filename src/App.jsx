df989d3a4ad9c315d1f2f60902fe7e2151ab9a74
johnj@John0609 <johnjairorodriguez384@gmail.com>
2023-05-02 22:12:18 -0500

    header working

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import './css/App.css'
-
 /*import { About, Contact, Experience, Feedbacks, Hero, Navbar, Stars, Tech, Works } from './components'*/
 import { Initial } from './pages/Home/Initial'
 const App = () => {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,75 @@
+import { useEffect, useState } from "react"
+import { Link } from "react-router-dom"
+
+import { stylesUsing } from "../styles"
+import { navLinks } from "../constants"
+
+import { logo, /*menu, close*/ } from '../assets'
+
 const Navbar = () => {
+  const [active, setActive] = useState("")
+
+  /*
+    useEffect(()=> {
+      window.addEventListener("scroll", () => {
+        if (window.scrollY > 100) {
+          document.querySelector("nav").className = `
+          ${stylesUsing.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary
+          shadow-md
+          `
+        } else {
+          document.querySelector("nav").className = `
+          ${stylesUsing.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary
+          `
+        }
+      })
+    })
+  */
   return (
-    <div>Navbar</div>
+    <nav className={`
+      ${stylesUsing.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary
+    `}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            window.href = "/"
+            window.scrollTo(0, 0)
+          }}
+
+        >
+          <img src={logo} alt="logo" className="w-12 h-12 object-contain rounded-xl" />
+          <p className="text-white text-[1.1] font-bold cursor-pointer">
+            John
+            <span className="hidden md:block">
+              | Software Developer
+            </span>
+            <span className="block md:hidden">
+              | SD
+            </span>
+          </p>
+
+        </Link>
+        <ul className="list-none  sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}
+                className={`
+             ${active === link.title ? "text-secondary" : "text-white"} hover:text-white text-[1.1rem] font-medium cursor-pointer`}
+
+                onClick={() => {
+                  setActive(link.title)
+
+                }}
+              >
+                {link.title}
+              </a>
+            </li>
+          ))}
+
+        </ul>
+      </div>
+    </nav>
   )
 }
 
